98d9b4df0aec64647496660e74de3a4625825386
Xiao Gui <xgui3783@gmail.com>
2019-12-30 15:23:59 +0100

    debug github actions

---
+++ main_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	// check that test files can be read
+	cube, err := ioutil.ReadFile("./testData/cube.stl")
+	if err != nil {
+		t.Errorf("reading testData/cube.stl fail. test data not readable")
+	}
+	if string(cube[:5]) != "solid" {
+		t.Errorf("cube.stl first 5 characters does not equal solid")
+	}
+}
