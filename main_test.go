65302307b9acede7f6fd0c1c490f792ca2536923
Xiao Gui <xgui3783@gmail.com>
2019-12-30 15:39:03 +0100

    github actions debug

--- main_test.go
+++ main_test.go
@@ -12,6 +12,6 @@ func TestMain(t *testing.T) {
 		t.Errorf("reading testData/cube.stl fail. test data not readable")
 	}
 	if string(cube[:5]) != "solid" {
-		t.Errorf("cube.stl first 5 characters does not equal solid, instead it is: %v", string(cube[:5]))
+		t.Errorf("cube.stl first 5 characters does not equal solid, instead it is: %v\nEntire file is:\n%v", string(cube[:5]), string(cube))
 	}
 }
